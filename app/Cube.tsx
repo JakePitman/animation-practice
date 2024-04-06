25cadc33bdc6bac36041d08cd8b07829f1880f5a
Jake Pitman <jakeseanp@gmail.com>
2024-04-06 18:04:36 +1300

    Handle animations with stop and start

--- app/Cube.tsx
+++ app/Cube.tsx
@@ -1,17 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 import { useAnimationContext } from "./AnimationContext";
+import { AnimationAction } from "three";
 
 export const Cube = () => {
   const { animationName } = useAnimationContext();
   // @ts-ignore
   const { nodes, animations } = useGLTF("/scene.glb");
   const { ref, mixer, names, actions, clips } = useAnimations(animations);
+  const currentAnimation = useRef<AnimationAction | null>(null);
 
   useEffect(() => {
     const animation = actions[animationName];
     if (actions && animation) {
+      if (currentAnimation.current) {
+        currentAnimation.current.stop();
+      }
       animation.play();
+      currentAnimation.current = animation;
     }
   }, [animationName, actions]);
 
